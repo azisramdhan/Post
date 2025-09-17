aebf22e539e243b382b5988520bd312eeb8e4b97
Azis R <alfatihstudi@gmail.com>
2025-09-17 19:42:37 +0700

    Initial commit

---
+++ Post/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Post
+//
+//  Created by Azis Ramdhan on 17/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
